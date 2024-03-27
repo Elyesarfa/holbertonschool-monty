f94623638830432e40f46a24b30c170b071bc2f7
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-28 00:01:06 +0100

    test

--- monty.c
+++ monty.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*
+*
+*/
 
-int main() {
-    FILE *file = fopen("bytecodes/nop_test.m", "r");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL) {
         fprintf(stderr, "Error opening file\n");
         exit(EXIT_FAILURE);
@@ -14,11 +22,13 @@ int main() {
 
     char opcode[10];
     int arg;
+    int line_number = 0;
 
     while (fscanf(file, "%s", opcode) != EOF) {
+        line_number++;
         if (strcmp(opcode, "push") == 0) {
             if (fscanf(file, "%d", &arg) != 1) {
-                fprintf(stderr, "Error: usage: push integer\n");
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
                 exit(EXIT_FAILURE);
             }
             push(arg);
@@ -27,7 +37,7 @@ int main() {
         } else if (strcmp(opcode, "nop") == 0) {
             nop();
         } else {
-            fprintf(stderr, "Unknown opcode: %s\n", opcode);
+            fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
             exit(EXIT_FAILURE);
         }
     }
