601182e25b9af74e415e0b041a9112158763ea81
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-28 20:49:10 +0100

    adding pop file

---
+++ pop.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * _pop - Removes the top element of the stack.
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+    
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    *stack = (*stack)->next;
+    if (*stack != NULL)
+        (*stack)->prev = NULL;
+    free(tmp);
+}
