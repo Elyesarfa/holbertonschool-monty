901cca939f0abe78ea938b7f5486e39367a8c7ad
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-28 20:33:36 +0100

    adding more files

--- function.c
+++ function.c
@@ -1,31 +1,41 @@
 #include "monty.h"
+
 /**
- * _push - Pushes an element to the stack.
+ * _push - add new element to the stack
  * @stack: Pointer to the top of the stack
  * @line_number: Line number being executed
  */
- 
 void _push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *newnode;
+    stack_t *newnode = malloc(sizeof(stack_t));
+    char *d = "$ \n\t", *op;
     int data;
-    char *op;
 
-    op = strtok(NULL, "$ \n\t");
-    if (op == NULL || !isdigit(*op))
-    {
-        fprintf(stderr, "L%d: usage: push integer", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    data = atoi(op);
-    newnode = malloc(sizeof(stack_t));
     if (newnode == NULL)
     {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
 
+    op = strtok(NULL, d);
+    if (op == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        free(newnode);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; op[i] != '\0'; i++)
+    {
+        if (!isdigit(op[i]) && !(op[i] == '-' && i == 0))
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            free(newnode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    data = atoi(op);
     newnode->n = data;
     newnode->next = *stack;
     newnode->prev = NULL;
---
+++ push.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * _push - Pushes an element to the stack.
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+    stack_t *newnode;
+    int data;
+    char *op;
+
+    op = strtok(NULL, " \n\t");
+    if (op == NULL || !isdigit(*op))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    data = atoi(op);
+    newnode = malloc(sizeof(stack_t));
+    if (newnode == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newnode->n = data;
+    newnode->next = *stack;
+    newnode->prev = NULL;
+
+    if (*stack != NULL)
+        (*stack)->prev = newnode;
+
+    *stack = newnode;
+}
