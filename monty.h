0c368738a4524439e6c276e1c47d8e965c701046
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-28 21:05:02 +0100

    adding swap file

--- monty.h
+++ monty.h
@@ -50,6 +50,7 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
 
 extern stack_t *head;
 
